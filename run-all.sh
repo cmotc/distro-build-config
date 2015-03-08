d4e928f115461eb9afd92da71f3f117064ee1838
cmotc <cmotc@openmailbox.org>
2015-03-08 01:06:29 -0500

    test 6, manually add apt-transport-https to includes.bootstrap

--- run-all.sh
+++ run-all.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-./step0-lb_clean.sh && ./step1-lb_config.sh && ./step2-get_keys.sh && ./step3-source_lists.sh && ./step4-package_lists.sh && ./step5-etc_settings.sh && ./step6-user_settings.sh && ./step7-setup_hooks.sh
+./step0-lb_clean.sh && ./step1-lb_config.sh && ./boostrap-https-transport.sh && ./step2-get_keys.sh && ./step3-source_lists.sh && ./step4-package_lists.sh && ./step5-etc_settings.sh && ./step6-user_settings.sh && ./step7-setup_hooks.sh
