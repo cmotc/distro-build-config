b753bd0d9d062d3307502e478e45f9f00be07c58
cmotc <cmotc@openmailbox.org>
2015-03-18 07:24:22 -0400

    battery dying

--- step1-lb_config.sh
+++ step1-lb_config.sh
@@ -5,5 +5,5 @@ echo ":'
 ##This first step generates a Live-Build configuration tree.
 '"
 #
-lb config -a amd64 -b iso-hybrid -d jessie --mirror-bootstrap "https://www.mirror.cse.unsw.edu.au/debian/" --mirror-chroot "https://www.mirror.cse.unsw.edu.au/debian/" --apt-options "--force-yes --assume-yes"
+lb config -a amd64 -b iso-hybrid -d jessie --mirror-bootstrap "https://mirror.cse.unsw.edu.au/debian/" --mirror-chroot "https://mirror.cse.unsw.edu.au/debian/" --apt-options "--force-yes --assume-yes"
 
