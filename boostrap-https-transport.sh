9f984836ad0d2b753f198c8ccea1bb7c57b43ddb
cmotc <cmotc@openmailbox.org>
2015-03-18 07:26:04 -0400

    battery dying

--- boostrap-https-transport.sh
+++ boostrap-https-transport.sh
@@ -1,25 +1,25 @@
 #! /bin/sh
-mkdir -p config/includes.bootstrap/usr/lib/apt/methods
-cp -p /usr/lib/apt/methods/https config/includes.bootstrap/usr/lib/apt/methods/https
-ls -la config/includes.bootstrap/usr/lib/apt/methods
-mkdir -p config/includes.bootstrap/usr/share/doc/apt-transport-https
-cp -p /usr/share/doc/apt-transport-https/NEWS.Debian.gz config/includes.bootstrap/usr/share/doc/apt-transport-https/NEWS.Debian.gz
-cp -p /usr/share/doc/apt-transport-https/apt-transport-https.README config/includes.bootstrap/usr/share/doc/apt-transport-https/apt-transport-https.README
-cp -p /usr/share/doc/apt-transport-https/changelog.gz config/includes.bootstrap/usr/share/doc/apt-transport-https/changelog.gz
-cp -p /usr/share/doc/apt-transport-https/copyright config/includes.bootstrap/usr/share/doc/apt-transport-https/copyright
-ls -la config/includes.bootstrap/usr/share/doc/apt-transport-https/
-
-mkdir -p config/includes.bootstrap/usr/lib/x86_64-linux-gnu
-cp -p /usr/lib/x86_64-linux-gnu/libcurl-gnutls.so.3 /usr/lib/x86_64-linux-gnu/libcurl-gnutls.so.3
-cp -p /usr/lib/x86_64-linux-gnu/libcurl-gnutls.so.4 /usr/lib/x86_64-linux-gnu/libcurl-gnutls.so.4
-cp -p /usr/lib/x86_64-linux-gnu/libcurl-gnutls.so.4.3.0 /usr/lib/x86_64-linux-gnu/libcurl-gnutls.so.4.3.0
-ls -la config/includes.bootstrap/usr/lib/x86_64-linux-gnu
-mkdir -p config/includes.bootstrap/usr/share/doc/libcurl3-gnutls 
-cp -p /usr/share/doc/libcurl3-gnutls/NEWS.Debian.gz config/includes.bootstrap/usr/share/doc/libcurl3-gnutls/NEWS.Debian.gz
-cp -p /usr/share/doc/libcurl3-gnutls/changelog.Debian.gz config/includes.bootstrap/usr/share/doc/libcurl3-gnutls/changelog.Debian.gz
-cp -p /usr/share/doc/libcurl3-gnutls/changelog.gz config/includes.bootstrap/usr/share/doc/libcurl3-gnutls/changelog.gz
-cp -p /usr/share/doc/libcurl3-gnutls/copyright config/includes.bootstrap/usr/share/doc/libcurl3-gnutls/copyright
-ls -la config/includes.bootstrap/usr/share/doc/libcurl3-gnutls 
-mkdir -p config/includes.bootstrap/usr/share/lintian/overrides 
-cp -p /usr/share/lintian/overrides/libcurl3-gnutls config/includes.bootstrap/usr/share/lintian/overrides/libcurl3-gnutls
-ls -la config/includes.bootstrap/usr/share/lintian/overrides
+##mkdir -p config/includes.bootstrap/usr/lib/apt/methods
+##cp -p /usr/lib/apt/methods/https config/includes.bootstrap/usr/lib/apt/methods/https
+#ls -la config/includes.bootstrap/usr/lib/apt/methods
+#mkdir -p config/includes.bootstrap/usr/share/doc/apt-transport-https
+#cp -p /usr/share/doc/apt-transport-https/NEWS.Debian.gz config/includes.bootstrap/usr/share/doc/apt-transport-https/NEWS.Debian.gz
+#cp -p /usr/share/doc/apt-transport-https/apt-transport-https.README config/includes.bootstrap/usr/share/doc/apt-transport-https/apt-transport-https.README
+#cp -p /usr/share/doc/apt-transport-https/changelog.gz config/includes.bootstrap/usr/share/doc/apt-transport-https/changelog.gz
+#cp -p /usr/share/doc/apt-transport-https/copyright config/includes.bootstrap/usr/share/doc/apt-transport-https/copyright
+#ls -la config/includes.bootstrap/usr/share/doc/apt-transport-https/
+#
+#mkdir -p config/includes.bootstrap/usr/lib/x86_64-linux-gnu
+#cp -p /usr/lib/x86_64-linux-gnu/libcurl-gnutls.so.3 /usr/lib/x86_64-linux-gnu/libcurl-gnutls.so.3
+#cp -p /usr/lib/x86_64-linux-gnu/libcurl-gnutls.so.4 /usr/lib/x86_64-linux-gnu/libcurl-gnutls.so.4
+#cp -p /usr/lib/x86_64-linux-gnu/libcurl-gnutls.so.4.3.0 /usr/lib/x86_64-linux-gnu/libcurl-gnutls.so.4.3.0
+#ls -la config/includes.bootstrap/usr/lib/x86_64-linux-gnu
+#mkdir -p config/includes.bootstrap/usr/share/doc/libcurl3-gnutls 
+#cp -p /usr/share/doc/libcurl3-gnutls/NEWS.Debian.gz config/includes.bootstrap/usr/share/doc/libcurl3-gnutls/NEWS.Debian.gz
+#cp -p /usr/share/doc/libcurl3-gnutls/changelog.Debian.gz config/includes.bootstrap/usr/share/doc/libcurl3-gnutls/changelog.Debian.gz
+#cp -p /usr/share/doc/libcurl3-gnutls/changelog.gz config/includes.bootstrap/usr/share/doc/libcurl3-gnutls/changelog.gz
+#cp -p /usr/share/doc/libcurl3-gnutls/copyright config/includes.bootstrap/usr/share/doc/libcurl3-gnutls/copyright
+#ls -la config/includes.bootstrap/usr/share/doc/libcurl3-gnutls 
+#mkdir -p config/includes.bootstrap/usr/share/lintian/overrides 
+#cp -p /usr/share/lintian/overrides/libcurl3-gnutls config/includes.bootstrap/usr/share/lintian/overrides/libcurl3-gnutls
+#ls -la config/includes.bootstrap/usr/share/lintian/overrides
