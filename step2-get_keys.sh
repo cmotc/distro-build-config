a74e8a1e1544ee8d4e73dd8cc592770f9103e7c3
cmotc <cmotc@openmailbox.org>
2015-05-22 02:54:12 -0400

    fixed freepto key problem

--- step2-get_keys.sh
+++ step2-get_keys.sh
@@ -9,7 +9,7 @@ echo ":'
 
 torsocks gpg --keyserver keys.gnupg.net --recv 886DDD89
 gpg --export 886DDD89 > config/archives/tor.list.key.chroot
-cp config/archives/tor.list.key.chroot config/archives/tor.list.key.binary
+#cp config/archives/tor.list.key.chroot config/archives/tor.list.key.binary
 
 echo ":'
 ###Downloading the i2p repository keys
@@ -17,28 +17,28 @@ echo ":'
 
 torsocks wget https://geti2p.net/_static/i2p-debian-repo.key.asc -O config/archives/i2p2.asc
 mv config/archives/i2p2.asc config/archives/i2p2.list.key.chroot
-cp config/archives/i2p2.list.key.chroot config/archives/i2p2.list.key.binary
+#cp config/archives/i2p2.list.key.chroot config/archives/i2p2.list.key.binary
 
 echo ":'
 ###Downloading the namecoin repository keys
 '"
 
 wget http://download.opensuse.org/repositories/home:p_conrad:coins/Debian_8.0/Release.key -O config/archives/namecoin.list.key.chroot
-cp config/archives/namecoin.list.key.chroot config/archives/namecoin.list.key.binary
+#cp config/archives/namecoin.list.key.chroot config/archives/namecoin.list.key.binary
 
 echo ":'
 ###Downloading the freepto repository keys
 '"
 
-wget https://github.com/boyska/freepto-lb/blob/r-berenjena/config/archives/freepto.list.key.chroot -O config/archives/freepto.list.key.chroot
-cp config/archives/freepto.list.key.chroot config/archives/freepto.list.key.binary
+wget https://raw.githubusercontent.com/boyska/freepto-lb/r-berenjena/config/archives/freepto.list.key.chroot -O config/archives/freepto.list.key.chroot
+#cp config/archives/freepto.list.key.chroot config/archives/freepto.list.key.binary
 
 echo ":'
 ###Downloading the Tox.im repository keys
 '"
 
 torsocks wget https://repo.tox.im/pubkey.gpg -O config/archives/toxim.list.key.chroot
-cp config/archives/toxim.list.key.chroot config/archives/toxim.list.key.binary
+#cp config/archives/toxim.list.key.chroot config/archives/toxim.list.key.binary
 
 echo ":'
 ###Downloading the Twister repository keys
@@ -46,4 +46,4 @@ echo ":'
 
 torsocks gpg --keyserver keyserver.ubuntu.com --recv-keys 9F5116BD
 gpg --export 9F5116BD > config/archives/twister.list.key.chroot
-cp config/archives/twister.list.key.chroot config/archives/twister.list.key.binary
+#cp config/archives/twister.list.key.chroot config/archives/twister.list.key.binary
