d3d907c1fc9ba6a059109b98f7f7d0b8fcb4b646
cmotc <cmotc@openmailbox.org>
2015-05-31 01:16:44 -0400

    fixed missing ifcpu64.c32 boot module, working on boot menu customization

--- update-bootmodules.sh
+++ update-bootmodules.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 rm ./local/live-build/share/bootloaders/syslinux/*.c32
+ln -s /usr/lib/syslinux/modules/bios/ifcpu64.c32 ./local/live-build/share/bootloaders/syslinux/ifcpu64.c32
 ln -s /usr/lib/syslinux/modules/bios/hdt.c32 ./local/live-build/share/bootloaders/syslinux/hdt.c32
 ln -s /usr/lib/syslinux/modules/bios/ldlinux.c32 ./local/live-build/share/bootloaders/syslinux/ldlinux.c32
 ln -s /usr/lib/syslinux/modules/bios/libcom32.c32 ./local/live-build/share/bootloaders/syslinux/libcom32.c32
