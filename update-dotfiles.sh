9898ad541df66d3cec90f878f452ba563746d985
dyngar <dyngar@dyngar.local>
2015-11-29 22:47:03 -0500

    removed superfluous old scripts. better ways now.

--- update-dotfiles.sh
+++
@@ -1,3 +0,0 @@
-cd config/includes.chroot/etc/skel
-rm .bashrc .bash_aliases .conkyrc .dmrc .gitconfig .gnomerc .profile .xinitrc .Xsession
-cp ~/.bashrc ~/.bash_aliases ~/.conkyrc ~/.dmrc ~/.gitconfig ~/.profile ~/.xinitrc ~/.Xsession ./
