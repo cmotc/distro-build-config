9e44a1063dfe095d58502d6b08f9eeec5f4f1eb2
dyngar <dyngar@dyngar.local>
2015-12-08 01:46:32 -0500

    updated repos to include my personal stuff, mostly desktop and my working copy of ratox

--- regen/step2-get_keys.sh
+++ regen/step2-get_keys.sh
@@ -1,49 +1,26 @@
 #! /bin/sh
-echo ":'
-##Getting the Supplemental Repository Keys
-'"
-
-echo ":'
-###Downloading the Tor repository keys
-'"
-
+echo "##Getting the Supplemental Repository Keys"
+echo "###Downloading the Tor repository keys"
 torsocks gpg --keyserver keys.gnupg.net --recv 886DDD89
 gpg --export 886DDD89 > config/archives/tor.list.key.chroot
 #cp config/archives/tor.list.key.chroot config/archives/tor.list.key.binary
 
-echo ":'
-###Downloading the i2p repository keys
-'"
-
+echo "###Downloading the i2p repository keys"
 torsocks wget https://geti2p.net/_static/i2p-debian-repo.key.asc -O config/archives/i2p2.asc
 mv config/archives/i2p2.asc config/archives/i2p2.list.key.chroot
 #cp config/archives/i2p2.list.key.chroot config/archives/i2p2.list.key.binary
 
-echo ":'
-###Downloading the namecoin repository keys
-'"
-
+echo "###Downloading the namecoin repository keys"
 wget http://download.opensuse.org/repositories/home:p_conrad:coins/Debian_8.0/Release.key -O config/archives/namecoin.list.key.chroot
 #cp config/archives/namecoin.list.key.chroot config/archives/namecoin.list.key.binary
 
-echo ":'
-###Downloading the freepto repository keys
-'"
-
-wget https://raw.githubusercontent.com/boyska/freepto-lb/r-berenjena/config/archives/freepto.list.key.chroot -O config/archives/freepto.list.key.chroot
-#cp config/archives/freepto.list.key.chroot config/archives/freepto.list.key.binary
-
-echo ":'
-###Downloading the Tox.im repository keys
-'"
-
-torsocks wget https://repo.tox.im/pubkey.gpg -O config/archives/toxim.list.key.chroot
+echo "###Downloading the Tox.im repository keys"
+torsocks wget https://pkg.tox.chat/debian/pkg.gpg.key -O config/archives/toxim.list.key.chroot
 #cp config/archives/toxim.list.key.chroot config/archives/toxim.list.key.binary
 
-echo ":'
-###Downloading the Twister repository keys
-'"
-
-torsocks gpg --keyserver keyserver.ubuntu.com --recv-keys 9F5116BD
-gpg --export 9F5116BD > config/archives/twister.list.key.chroot
+echo "###Downloading the Fireaxe repository keys"
+torsocks wget -qO - https://cmotc.github.io/apt-git/cmotc.github.io.gpg.key -O config/archives/cmotc.github.io.gpg.key.chroot
+#echo "###Downloading the Twister repository keys"
+#torsocks gpg --keyserver keyserver.ubuntu.com --recv-keys 9F5116BD
+#gpg --export 9F5116BD > config/archives/twister.list.key.chroot
 #cp config/archives/twister.list.key.chroot config/archives/twister.list.key.binary
--- regen/step3-source_lists.sh
+++ regen/step3-source_lists.sh
@@ -1,86 +1,53 @@
 #! /bin/sh
-echo ":'
-##Crafting the sources.list entries
-'"
+echo "##Crafting the sources.list entries"
 
-echo "
-#deb https://mirror.cse.unsw.edu.au/debian/ jessie main
+echo "#deb https://mirror.cse.unsw.edu.au/debian/ jessie main
 #deb-src https://mirror.cse.unsw.edu.au/debian/ jessie main
-
 deb https://mirror.cse.unsw.edu.au/debian-security/ jessie/updates main
 deb-src https://mirror.cse.unsw.edu.au/debian-security/ jessie/updates main
-
 deb https://mirror.cse.unsw.edu.au/debian/ jessie-updates main
 deb-src https://mirror.cse.unsw.edu.au/debian/ jessie-updates main
-
 deb https://mirror.cse.unsw.edu.au/debian/ sid main
 deb-src https://mirror.cse.unsw.edu.au/debian/ sid main
-
 deb https://mirror.cse.unsw.edu.au/debian/ experimental main
 deb-src https://mirror.cse.unsw.edu.au/debian/ experimental main
 " > config/archives/debian.list.chroot
 cp config/archives/debian.list.chroot config/archives/debian.list.binary
 
-echo "
-deb http://deb.torproject.org/torproject.org jessie main
+echo "deb http://deb.torproject.org/torproject.org jessie main
 deb-src http://deb.torproject.org/torproject.org jessie main
 " > config/archives/tor.list.chroot
+cp config/archives/tor.list.chroot config/archives/tor.list.binary
 
-echo "
-deb http://deb.torproject.org/torproject.org jessie main
-deb-src http://deb.torproject.org/torproject.org jessie main
-" > config/archives/tor.list.binary
-
-echo '
-deb http://download.opensuse.org/repositories/home:/p_conrad:/coins/Debian_8.0/ /
+echo 'deb http://download.opensuse.org/repositories/home:/p_conrad:/coins/Debian_8.0/ /
 deb-src http://download.opensuse.org/repositories/home:/p_conrad:/coins/Debian_8.0/ /
 ' > config/archives/namecoin.list.chroot
+cp config/archives/namecoin.list.chroot config/archives/namecoin.list.binary
 
-echo '
-deb http://deb.freepto.mx/freeptorepo berenjena main
-' > config/archives/freepto.list.chroot
-
-echo '
-deb http://deb.freepto.mx/freeptorepo berenjena main
-' > config/archives/freepto.list.binary
-
-echo '
-deb http://download.opensuse.org/repositories/home:/p_conrad:/coins/Debian_7.0/ /
-deb-src http://download.opensuse.org/repositories/home:/p_conrad:/coins/Debian_7.0/ /
-' > config/archives/namecoin.list.binary
-
-echo "
-deb http://deb.i2p2.no/ unstable main
+echo "deb http://deb.i2p2.no/ unstable main
 deb-src http://deb.i2p2.no/ unstable main
 " > config/archives/i2p2.list.chroot
+cp config/archives/i2p2.list.chroot config/archives/i2p2.list.binary
 
-echo "
-deb http://deb.i2p2.no/ unstable main
-deb-src http://deb.i2p2.no/ unstable main
-" > config/archives/i2p2.list.binary
-
-echo "
-deb http://repo.tox.im/ nightly main
+echo "deb https://pkg.tox.chat/debian nightly main
 " > config/archives/toxim.list.chroot
+cp config/archives/toxim.list.chroot config/archives/toxim.list.binary
 
-echo "
-deb http://repo.tox.im/ nightly main
-" > config/archives/toxim.list.binary
+echo "deb https://cmotc.github.io/apt-git/debian/ testing main
+" > config/archives/cmotc.github.io.list.chroot
+cp config/archives/cmotc.github.io.list.chroot config/archives/cmotc.github.io.list.binary
 
-echo "
-deb http://ppa.launchpad.net/podshumok/twister-core-daily/ubuntu trusty main 
-deb-src http://ppa.launchpad.net/podshumok/twister-core-daily/ubuntu trusty main 
-" > config/archives/twister.list.chroot
-cp config/archives/twister.list.chroot config/archives/twister.list.binary
+#echo "deb http://ppa.launchpad.net/podshumok/twister-core-daily/ubuntu trusty main 
+#deb-src http://ppa.launchpad.net/podshumok/twister-core-daily/ubuntu trusty main 
+#" > config/archives/twister.list.chroot
+#cp config/archives/twister.list.chroot config/archives/twister.list.binary
 
 echo "Package: *
 Pin: release a=jessie
 Pin-Priority: 900
-
 Package: *
 Pin: release a=sid
 Pin-Priority: 600
-
 Package: *
 Pin: release a=experimental
 Pin-Priority: 500
@@ -90,47 +57,36 @@ cp config/archives/debian.pref.chroot config/archives/debian.pref.binary
 echo "Package: brasero
 Pin: version *
 Pin-Priority: -1
-
 Package: k3b
 Pin: version *
 Pin-Priority: -1
-
 Package: exfalso
 Pin: version *
 Pin-Priority: -1
-
 Package: uzbl
 Pin: version *
 Pin-Priority: -1
-
 Package: xterm
 Pin: version *
 Pin-Priority: -1
-
 Package: uxterm
 Pin: version *
 Pin-Priority: -1
-
 Package: vala-terminal
 Pin: version *
 Pin-Priority: -1
-
 Package: libreoffice-base
 Pin: version *
 Pin-Priority: -1
-
 Package: exim4-daemon-light
 Pin: version *
 Pin-Priority: -1
-
 Package: exim4-daemon-heavy
 Pin: version *
 Pin-Priority: -1
-
 Package: minissdpd
 Pin: version *
 Pin-Priority: -1
-
 Package: epdfview
 Pin: version *
 Pin-Priority: -1
